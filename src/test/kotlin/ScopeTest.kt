766bbbe371d25bfe2ee1b5a962d741bb021f7cc1
shinyay <shinya.com@gmail.com>
2020-08-03 23:13:27 +0900

    Add Scope Function test for apply

--- src/test/kotlin/ScopeTest.kt
+++ src/test/kotlin/ScopeTest.kt
@@ -1,2 +1,19 @@
+import com.google.shinyay.model.Employee
+import com.google.shinyay.model.Gender
+import org.junit.Assert
+import org.junit.Test
+
 class ScopeTest {
+
+    @Test
+    fun testApplyFunction() {
+        val employee: Employee = Employee("Shinya", lastName = "Yanagihara", id = 1)
+            .apply {
+                age = 40
+                gender = Gender.MALE
+            }
+        Assert.assertEquals(40, employee.age)
+        Assert.assertEquals(Gender.MALE, employee.gender)
+    }
+
 }
