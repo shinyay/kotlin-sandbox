52765340fd7a2f0cbd63e84d11c256670bfd703e
shinyay <shinya.com@gmail.com>
2020-08-03 23:03:12 +0900

    Modify paramers to class parameter from constructor

--- src/main/kotlin/com/google/shinyay/model/Employee.kt
+++ src/main/kotlin/com/google/shinyay/model/Employee.kt
@@ -2,6 +2,6 @@ package com.google.shinyay.model
 
 data class Employee(val firstName: String,
                     val lastName: String,
-                    val id: Int,
-                    val age: Int,
-                    val gender: Gender)
+                    val id: Int) {
+    var age: Int = 0
+    var gender: Gender = Gender.MALE }
